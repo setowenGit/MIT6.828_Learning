7d1abf58a3491ee69e5ccb392efbcf54c50c9c43
setowenGit <setowen1013@sina.com>
2024-05-06 20:57:20 +0800

    new file:   LAB2/.dir-locals.el 	new file:   LAB2/.gdbinit.tmpl 	new file:   LAB2/.gitignore 	new file:   LAB2/CODING 	new file:   LAB2/GNUmakefile 	new file:   LAB2/boot/Makefrag 	new file:   LAB2/boot/boot.S 	new file:   LAB2/boot/main.c 	new file:   LAB2/boot/sign.pl 	new file:   LAB2/conf/env.mk 	new file:   LAB2/conf/lab.mk 	new file:   LAB2/core 	new file:   LAB2/fs/test.c 	new file:   LAB2/fs/testshell.key 	new file:   LAB2/grade-lab1 	new file:   LAB2/grade-lab2 	new file:   LAB2/gradelib.py 	new file:   LAB2/hw2_test 	new file:   LAB2/inc/COPYRIGHT 	new file:   LAB2/inc/assert.h 	new file:   LAB2/inc/elf.h 	new file:   LAB2/inc/error.h 	new file:   LAB2/inc/kbdreg.h 	new file:   LAB2/inc/memlayout.h 	new file:   LAB2/inc/mmu.h 	new file:   LAB2/inc/stab.h 	new file:   LAB2/inc/stdarg.h 	new file:   LAB2/inc/stdio.h 	new file:   LAB2/inc/string.h 	new file:   LAB2/inc/types.h 	new file:   LAB2/inc/x86.h 	new file:   LAB2/kern/COPYRIGHT 	new file:   LAB2/kern/Makefrag 	new file:   LAB2/kern/console.c 	new file:   LAB2/kern/console.h 	new file:   LAB2/kern/entry.S 	new file:   LAB2/kern/entrypgdir.c 	new file:   LAB2/kern/init.c 	new file:   LAB2/kern/kclock.c 	new file:   LAB2/kern/kclock.h 	new file:   LAB2/kern/kdebug.c 	new file:   LAB2/kern/kdebug.h 	new file:   LAB2/kern/kernel.ld 	new file:   LAB2/kern/monitor.c 	new file:   LAB2/kern/monitor.h 	new file:   LAB2/kern/pmap.c 	new file:   LAB2/kern/pmap.h 	new file:   LAB2/kern/printf.c 	new file:   LAB2/lib/printfmt.c 	new file:   LAB2/lib/readline.c 	new file:   LAB2/lib/string.c 	new file:   LAB2/mergedep.pl 	new file:   LAB2/mit_shell 	new file:   LAB2/sh.c 	new file:   LAB2/t.sh 	new file:   LAB2/user/sendpage.c

---
+++ LAB2/inc/memlayout.h
@@ -0,0 +1,222 @@
+#ifndef JOS_INC_MEMLAYOUT_H
+#define JOS_INC_MEMLAYOUT_H
+
+#ifndef __ASSEMBLER__
+#include <inc/types.h>
+#include <inc/mmu.h>
+#endif /* not __ASSEMBLER__ */
+
+/*
+ * This file contains definitions for memory management in our OS,
+ * which are relevant to both the kernel and user-mode software.
+ */
+
+// Global descriptor numbers
+#define GD_KT     0x08     // kernel text
+#define GD_KD     0x10     // kernel data
+#define GD_UT     0x18     // user text
+#define GD_UD     0x20     // user data
+#define GD_TSS0   0x28     // Task segment selector for CPU 0
+
+// KERNBASE以上的内存布局（注意这里的地址是物理地址，要换成虚拟地址则加KERNBASE）
+/*
+ *                     .                              .
+ *                     .       Managable Space        .
+ *                     .                              .
+pages ends 0x158000 -->+------------------------------+
+ *                     |                              |
+ *                     .                              .
+ *                     .   pages management array     .
+ *                     .                              .
+ *                     |                              |
+ *  pages 0x118000 ->  +------------------------------+
+ *                     |        Kernel is here        |
+ *    EXT 0x100000 ->  +------------------------------+
+ *                     |                              |
+ *                     |          IO Hole             |
+ *                     |                              |
+ * BASEMEM 0xa0000 ->  +------------------------------+
+ *                     |    Basic Managable Space     |
+ *    KERNBASE ----->  +------------------------------+
+ */
+
+/*
+ * Virtual memory map:                                Permissions
+ *                                                    kernel/user
+ *
+ *    4 Gig -------->  +------------------------------+
+ *                     |                              | RW/--
+ *                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *                     :              .               :
+ *                     :              .               :
+ *                     :              .               :
+ *                     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| RW/--
+ *                     |                              | RW/--
+ *                     |   Remapped Physical Memory   | RW/--
+ *                     |                              | RW/--
+ *    KERNBASE, ---->  +------------------------------+ 0xf0000000      --+
+ *    KSTACKTOP        |     CPU0's Kernel Stack      | RW/--  KSTKSIZE   |
+ *                     | - - - - - - - - - - - - - - -|                   |
+ *                     |      Invalid Memory (*)      | --/--  KSTKGAP    |
+ *                     +------------------------------+                   |
+ *                     |     CPU1's Kernel Stack      | RW/--  KSTKSIZE   |
+ *                     | - - - - - - - - - - - - - - -|                 PTSIZE
+ *                     |      Invalid Memory (*)      | --/--  KSTKGAP    |
+ *                     +------------------------------+                   |
+ *                     :              .               :                   |
+ *                     :              .               :                   |
+ *    MMIOLIM ------>  +------------------------------+ 0xefc00000      --+
+ *                     |       Memory-mapped I/O      | RW/--  PTSIZE
+ * ULIM, MMIOBASE -->  +------------------------------+ 0xef800000
+ *                     |  Cur. Page Table (User R-)   | R-/R-  PTSIZE
+ *    UVPT      ---->  +------------------------------+ 0xef400000
+ *                     |          RO PAGES            | R-/R-  PTSIZE
+ *    UPAGES    ---->  +------------------------------+ 0xef000000
+ *                     |           RO ENVS            | R-/R-  PTSIZE
+ * UTOP,UENVS ------>  +------------------------------+ 0xeec00000
+ * UXSTACKTOP -/       |     User Exception Stack     | RW/RW  PGSIZE
+ *                     +------------------------------+ 0xeebff000
+ *                     |       Empty Memory (*)       | --/--  PGSIZE
+ *    USTACKTOP  --->  +------------------------------+ 0xeebfe000
+ *                     |      Normal User Stack       | RW/RW  PGSIZE
+ *                     +------------------------------+ 0xeebfd000
+ *                     |                              |
+ *                     |                              |
+ *                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *                     .                              .
+ *                     .                              .
+ *                     .                              .
+ *                     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+ *                     |     Program Data & Heap      |
+ *    UTEXT -------->  +------------------------------+ 0x00800000
+ *    PFTEMP ------->  |       Empty Memory (*)       |        PTSIZE
+ *                     |                              |
+ *    UTEMP -------->  +------------------------------+ 0x00400000      --+
+ *                     |       Empty Memory (*)       |                   |
+ *                     | - - - - - - - - - - - - - - -|                   |
+ *                     |  User STAB Data (optional)   |                 PTSIZE
+ *    USTABDATA ---->  +------------------------------+ 0x00200000        |
+ *                     |       Empty Memory (*)       |                   |
+ *    0 ------------>  +------------------------------+                 --+
+ *
+ * (*) Note: The kernel ensures that "Invalid Memory" is *never* mapped.
+ *     "Empty Memory" is normally unmapped, but user programs may map pages
+ *     there if desired.  JOS user programs map pages temporarily at UTEMP.
+ */
+
+
+// All physical memory mapped at this address
+// 虚拟地址对于物理地址的偏移
+#define	KERNBASE	0xF0000000
+
+// At IOPHYSMEM (640K) there is a 384K hole for I/O.  From the kernel,
+// IOPHYSMEM can be addressed at KERNBASE + IOPHYSMEM.  The hole ends
+// at physical address EXTPHYSMEM.
+#define IOPHYSMEM	0x0A0000 // IO段内存的开头
+#define EXTPHYSMEM	0x100000 // 拓展区的开头，其上限由RAM硬件大小决定
+
+// Kernel stack.
+// 内核栈
+#define KSTACKTOP	KERNBASE
+#define KSTKSIZE	(8*PGSIZE)   		// size of a kernel stack 内核栈空间 32KB
+#define KSTKGAP		(8*PGSIZE)   		// size of a kernel stack guard
+
+// Memory-mapped IO.
+#define MMIOLIM		(KSTACKTOP - PTSIZE)
+#define MMIOBASE	(MMIOLIM - PTSIZE)
+
+#define ULIM		(MMIOBASE)
+
+/*
+ * User read-only mappings! Anything below here til UTOP are readonly to user.
+ * They are global pages mapped in at env allocation time.
+ */
+
+// User read-only virtual page table (see 'uvpt' below)
+#define UVPT		(ULIM - PTSIZE)
+// Read-only copies of the Page structures
+// 页面结构的只读副本
+#define UPAGES		(UVPT - PTSIZE)
+// Read-only copies of the global env structures
+#define UENVS		(UPAGES - PTSIZE)
+
+/*
+ * Top of user VM. User can manipulate VA from UTOP-1 and down!
+ */
+
+// Top of user-accessible VM
+#define UTOP		UENVS
+// Top of one-page user exception stack
+#define UXSTACKTOP	UTOP
+// Next page left invalid to guard against exception stack overflow; then:
+// Top of normal user stack
+#define USTACKTOP	(UTOP - 2*PGSIZE)
+
+// Where user programs generally begin
+#define UTEXT		(2*PTSIZE)
+
+// Used for temporary page mappings.  Typed 'void*' for convenience
+#define UTEMP		((void*) PTSIZE)
+// Used for temporary page mappings for the user page-fault handler
+// (should not conflict with other temporary page mappings)
+#define PFTEMP		(UTEMP + PTSIZE - PGSIZE)
+// The location of the user-level STABS data structure
+#define USTABDATA	(PTSIZE / 2)
+
+#ifndef __ASSEMBLER__
+
+typedef uint32_t pte_t;
+typedef uint32_t pde_t;
+
+#if JOS_USER
+/*
+ * The page directory entry corresponding to the virtual address range
+ * [UVPT, UVPT + PTSIZE) points to the page directory itself.  Thus, the page
+ * directory is treated as a page table as well as a page directory.
+ *
+ * One result of treating the page directory as a page table is that all PTEs
+ * can be accessed through a "virtual page table" at virtual address UVPT (to
+ * which uvpt is set in lib/entry.S).  The PTE for page number N is stored in
+ * uvpt[N].  (It's worth drawing a diagram of this!)
+ *
+ * A second consequence is that the contents of the current page directory
+ * will always be available at virtual address (UVPT + (UVPT >> PGSHIFT)), to
+ * which uvpd is set in lib/entry.S.
+ */
+extern volatile pte_t uvpt[];     // VA of "virtual page table"
+extern volatile pde_t uvpd[];     // VA of current page directory
+#endif
+
+/*
+ * Page descriptor structures, mapped at UPAGES.
+ * Read/write to the kernel, read-only to user programs.
+ *
+ * Each struct PageInfo stores metadata for one physical page.
+ * Is it NOT the physical page itself, but there is a one-to-one
+ * correspondence between physical pages and struct PageInfo's.
+ * You can map a struct PageInfo * to the corresponding physical address
+ * with page2pa() in kern/pmap.h.
+ * 页描述符结构，在UPAGES映射。
+ * 对内核是读/写，对用户程序是只读。
+ *
+ * 每个结构PageInfo存储一个物理页的元数据。
+ * 它不是物理页本身，但是物理页和结构PageInfo之间有一个一对一的对应关系。
+ * 你可以将一个PageInfo结构映射到相应的物理地址, 使用kern/pmap.h中的page2pa()。
+ */
+struct PageInfo {
+	// Next page on the free list.
+	// 空闲列表中的下一页
+	struct PageInfo *pp_link;
+
+	// pp_ref is the count of pointers (usually in page table entries)
+	// to this page, for pages allocated using page_alloc.
+	// Pages allocated at boot time using pmap.c's
+	// boot_alloc do not have valid reference count fields.
+	// pp_ref是指使用page_alloc分配的页面，指向该页面的指针（通常在页表项中）的数量。 
+    // 使用pmap.c的boot_alloc在启动时分配的页面没有有效的引用计数域。
+
+	uint16_t pp_ref;
+};
+
+#endif /* !__ASSEMBLER__ */
+#endif /* !JOS_INC_MEMLAYOUT_H */
